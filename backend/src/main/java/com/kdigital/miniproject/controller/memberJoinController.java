2a293c98f2c8b3d2ed0d1909aff877e78ee9c02d
sujidaddy <kyuhuhu.sujidaddy@gmail.com>
2025-12-25 21:55:03 +0900

    각 인증을 포함한 회원가입 처리
    ID/비번, Oauth2 인증을 통한 로그인 처리 추가
    
    로그인 시 로그인한 Member Data Atom에 저장 처리

--- backend/src/main/java/com/kdigital/miniproject/controller/memberJoinController.java
+++ backend/src/main/java/com/kdigital/miniproject/controller/memberJoinController.java
@@ -2,10 +2,7 @@ package com.kdigital.miniproject.controller;
 
 import java.time.LocalDateTime;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,17 +32,41 @@ public class memberJoinController {
 
 	// 등록된 ID 확인
 	@GetMapping("/v1/join/validateID/{id}")
-	public ResponseEntity<Object> getValiedID(@PathVariable String id) throws Exception {
-		String ret = memberRepo.findByUserid(id).isPresent() ? "False" : "True";
+	public ResponseEntity<Object> validateID(@PathVariable String id) throws Exception {
+		String ret = memberRepo.getByUserid(id).isPresent() ? "False" : "True";
 		System.out.println("validateID result : " + ret);
 		return ResponseEntity.ok(ret);
 	}
 	
 	// 등록된 이메일 확인
 	@GetMapping("/v1/join/validateEmail/{email}")
-	public ResponseEntity<Object> getValiedEmail(@PathVariable String email) throws Exception {
-		String ret = memberRepo.findByEmail(email).isPresent() ? "False" : "True";
+	public ResponseEntity<Object> validateEmail(@PathVariable String email) throws Exception {
+		String ret = memberRepo.getByEmail(email).isPresent() ? "False" : "True";
 		System.out.println("validateEmail result : " + ret);
+		return ResponseEntity.ok(ret);
+	}
+	
+	// 등록된 구글 계정 확인
+	@GetMapping("/v1/join/validateGoogle/{google}")
+	public ResponseEntity<Object> validateGoogle(@PathVariable String google) throws Exception {
+		String ret = memberRepo.getByGoogle(google).isPresent() ? "False" : "True";
+		System.out.println("validateGoogle result : " + ret);
+		return ResponseEntity.ok(ret);
+	}
+	
+	// 등록된 네이버 계정 확인
+	@GetMapping("/v1/join/validateNaver/{naver}")
+	public ResponseEntity<Object> validateNaver(@PathVariable String naver) throws Exception {
+		String ret = memberRepo.getByNaver(naver).isPresent() ? "False" : "True";
+		System.out.println("validateNaver result : " + ret);
+		return ResponseEntity.ok(ret);
+	}
+	
+	// 등록된 카카오 계정 확인
+	@GetMapping("/vi/join/validateKakao/{kakao}")
+	public ResponseEntity<Object> validateKakao(@PathVariable String kakao) throws Exception {
+		String ret = memberRepo.getByKakao(kakao).isPresent() ? "False" : "True";
+		System.out.println("validateKakao result : " + ret);
 		return ResponseEntity.ok(ret);
 	}
 	
@@ -56,7 +77,20 @@ public class memberJoinController {
 			@RequestParam("userid") String userid,
 			@RequestParam("password") String password,
 			@RequestParam("username") String username,
-			@RequestParam("email") String email) {
+			@RequestParam("email") String email,
+			@RequestParam("google") String google,
+			@RequestParam("naver") String naver,
+			@RequestParam("kakao") String kakao) {
+		if(memberRepo.getByUserid(userid).isPresent())
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("이미 가입한 ID 입니다");
+		if(memberRepo.getByEmail(email).isPresent())
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("이미 가입한 이메일 입니다");
+		if(google != null && memberRepo.getByGoogle(google).isPresent())
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("이미 인증한 구글계정 입니다");
+		if(naver != null && memberRepo.getByNaver(naver).isPresent())
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("이미 인증한 네이버계정 입니다");
+		if(kakao != null && memberRepo.getByKakao(kakao).isPresent())
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("이미 인증한 카카오계정 입니다");
 		Member member = Member.builder()
 							.userid(userid)
 							.password(encoder.encode(password))
@@ -67,10 +101,12 @@ public class memberJoinController {
 							.createTime(LocalDateTime.now())
 							.lastLoginTime(LocalDateTime.now())
 							.build();
-		if(memberRepo.findByUserid(userid).isPresent())
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이미 가입한 ID 입니다");
-		if(memberRepo.findByEmail(email).isPresent())
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이미 이메일 입니다");
+		if(google != null)
+			member.setGoogle(google);
+		if(naver != null)
+			member.setNaver(naver);
+		if(kakao != null)
+			member.setKakao(kakao);
 		memberRepo.save(member);
 		System.out.println("getJoinUser result : " + member.getUser_no());
 		// 로그인 기록 추가
--- backend/src/main/java/com/kdigital/miniproject/persistence/MemberRepository.java
+++ backend/src/main/java/com/kdigital/miniproject/persistence/MemberRepository.java
@@ -1,6 +1,5 @@
 package com.kdigital.miniproject.persistence;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,11 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kdigital.miniproject.domain.Member;
 
 
+
 public interface MemberRepository extends JpaRepository<Member, Long>{
 	Optional<Member> getByUsername(String username);
-	Optional<Member> findByUsername(String username);
-	Optional<Member> findByUserid(String userid);
-	Optional<Member> findByEmail(String email);
+	Optional<Member> getByUserid(String userid);
+	Optional<Member> getByEmail(String email);
 	Optional<Member> getByGoogle(String google);
 	Optional<Member> getByNaver(String naver);
 	Optional<Member> getByKakao(String kakao);
