f6d2f72fad128cf633ba99929b0b5a5e1c8cf413
sujidaddy <kyuhuhu.sujidaddy@gmail.com>
2026-01-22 14:07:16 +0900

    중첩된 파일 정리

--- backend/src/main/java/com/kdigital/miniproject/domain/TopLocationDTO.java
+++ backend/src/main/java/com/kdigital/miniproject/domain/TopLocationDTO.java
@@ -14,7 +14,6 @@ import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
